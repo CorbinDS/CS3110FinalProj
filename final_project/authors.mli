fbb5d6e12127f72ded67e491a8d1e04d1de40e4d
Nicole Cheetham <nicole@dhcp-vl2041-26158.eduroam.cornell.edu>
2022-03-16 22:41:12 -0400

    Created authors.ml and authors.mli

---
+++ final_project/authors.mli
@@ -0,0 +1,14 @@
+(************************************************************)
+
+(** CS 3110 Spring 2022 Final Project Assignment
+
+    @author Nicole Cheetham (nmc74)
+    @author Corbin Stickley (cds244)
+    @author Sanjana Nandi (sn523)
+*)
+
+
+(************************************************************)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours you worked on this assignment. *)
