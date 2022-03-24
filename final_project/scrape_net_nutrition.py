931d33989fb571eac99fdd27e4b3cfd1718f0160
Sanjana679 <sanjana@nandi.net>
2022-03-24 13:45:54 -0400

    Updated scrape net nutrition

--- final_project/scrape_net_nutrition.py
+++ final_project/scrape_net_nutrition.py
@@ -1,61 +1,68 @@
+import splinter
 from splinter import Browser
 import json
 
-browser = Browser()
-browser.visit('https://netnutrition.dining.cornell.edu/NetNutrition/1')
+from selenium.webdriver.firefox.options import Options
 
-item_ingredient_list = {}
+options = Options()
+options.headless = True
 
 
-def wait(tag):
+def wait(browser, tag):
     browser.is_element_not_present_by_tag(tag, wait_time=1)
 
 
-for e in range(len(browser.find_by_tag('a[class="cbo_nn_unitNameLink unit__name-link "]'))):
-    wait('a[class="cbo_nn_unitNameLink unit__name-link "]')
-    eatery = browser.find_by_tag(
-        'a[class="cbo_nn_unitNameLink unit__name-link "]')[e]
-    eatery.click()
+def update_net_nutrition():
+    browser = Browser(options=options)
 
-    for m in range(len(browser.find_by_tag('a[class="cbo_nn_menuLink"]'))):
-        wait('a[class="cbo_nn_menuLink"]')
-        menu = browser.find_by_tag('a[class="cbo_nn_menuLink"]')[m]
-        menu.click()
+    browser.visit('https://netnutrition.dining.cornell.edu/NetNutrition/1')
 
-        for i in range(len(browser.find_by_tag('a[class="cbo_nn_itemHover"]'))):
-            wait('a[class="cbo_nn_itemHover"]')
-            item = browser.find_by_tag('a[class="cbo_nn_itemHover"]')[i]
-            item.click()
+    item_ingredient_list = {}
 
-            wait('td[class="cbo_nn_LabelHeader"]')
-            item_name = browser.find_by_tag(
-                'td[class="cbo_nn_LabelHeader"]').value
+    for e in range(len(browser.find_by_tag('a[class="cbo_nn_unitNameLink unit__name-link "]'))):
+        wait(browser, 'a[class="cbo_nn_unitNameLink unit__name-link "]')
+        eatery = browser.find_by_tag(
+            'a[class="cbo_nn_unitNameLink unit__name-link "]')[e]
+        eatery.click()
 
-            try:
-                ingredients_label = browser.find_by_tag(
-                    'span[class="cbo_nn_LabelIngredients"]').value
-            except:
-                ingredients_label = ""
+        for m in range(len(browser.find_by_tag('a[class="cbo_nn_menuLink"]'))):
+            wait(browser, 'a[class="cbo_nn_menuLink"]')
+            menu = browser.find_by_tag('a[class="cbo_nn_menuLink"]')[m]
+            menu.click()
 
-            item_ingredient_list[item_name] = ingredients_label
-            wait('btn_nn_nutrition_close')
-            browser.find_by_id('btn_nn_nutrition_close').click()
-        wait(
-            'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]')
-        browser.find_by_tag(
-            'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]').click()
+            for i in range(len(browser.find_by_tag('a[class="cbo_nn_itemHover"]'))):
+                wait(browser, 'a[class="cbo_nn_itemHover"]')
+                item = browser.find_by_tag('a[class="cbo_nn_itemHover"]')[i]
+                item.click()
 
-    wait(
-        'a[onclick="NetNutrition.UI.courseListBackBtn(event)"]')
-    try:
-        browser.find_by_tag(
-            'a[onclick="NetNutrition.UI.courseListBackBtn(event)"]').click()
-    except:
-        browser.find_by_tag(
-            'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]').click()
+                wait(browser, 'td[class="cbo_nn_LabelHeader"]')
+                item_name = browser.find_by_tag(
+                    'td[class="cbo_nn_LabelHeader"]').value
 
+                try:
+                    ingredients_label = browser.find_by_tag(
+                        'span[class="cbo_nn_LabelIngredients"]').value
+                except:
+                    ingredients_label = ""
 
-with open('net_nutrition.json', 'w') as json_file:
-    json.dump(item_ingredient_list, json_file)
+                item_ingredient_list[item_name] = ingredients_label
+                wait(browser, 'btn_nn_nutrition_close')
+                browser.find_by_id('btn_nn_nutrition_close').click()
+            wait(browser,
+                 'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]')
+            browser.find_by_tag(
+                'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]').click()
 
-browser.quit()
+        wait(browser,
+             'a[onclick="NetNutrition.UI.courseListBackBtn(event)"]')
+        try:
+            browser.find_by_tag(
+                'a[onclick="NetNutrition.UI.courseListBackBtn(event)"]').click()
+        except:
+            browser.find_by_tag(
+                'a[onclick="NetNutrition.UI.menuDetailBackBtn()"]').click()
+
+    with open('net_nutrition.json', 'w') as json_file:
+        json.dump(item_ingredient_list, json_file)
+
+    browser.quit()
