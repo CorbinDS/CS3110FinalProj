42b78337848cc4d1d8f424b9ba6009e60049a1e2
Sanjana679 <sanjana@nandi.net>
2022-03-28 02:37:59 -0400

    new database changes

--- final_project/_build/default/bin/main.ml
+++ final_project/_build/default/bin/main.ml
@@ -2,4 +2,6 @@ open Final_project
 open Draw
 open Database
 
-let _ = List.map (fun m -> print_endline (pretty_print_menu m)) menus
+let _ =
+  filter_menus (Item "sausage") menus
+  |> List.map (fun m -> print_endline (pretty_print_menu m))
